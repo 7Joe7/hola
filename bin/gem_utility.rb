b3e3446926773b320ac24ef024f2493b4bd8c135
Joe <josef.erneker@gmail.com>
2014-12-26 15:42:26 +0100

    Build gem version

--- bin/gem_utility.rb
+++ bin/gem_utility.rb
@@ -37,7 +37,7 @@ if gem_name
   # run the gem build and parse for the gem release filename
   gem_build_name = `gem build #{gem_name}.gemspec`
   puts $2
-  puts `$2`
+  puts `awk '/File/ {print $2}`
 
   # if the build failed (i.e. no file name obtained above), print error message and exit
   if gem_build_name
