1cbb2ce8e1b2ccacc207f362cf202168adc8bc63
Joe <josef.erneker@gmail.com>
2014-12-28 15:39:17 +0100

    Test proj_util

--- test/test_proj_util.rb
+++ test/test_proj_util.rb
@@ -4,20 +4,17 @@ require 'joe_utils/utils/project_utility.rb'
 class JoeUtilsTest < Minitest::Test
 
   def test_commit
-    # assert_equal(true, ProjectUtility.new.run(['-s', 'Test proj_util']))
-    result = ProjectUtility.new.run(['-s', 'Test proj_util'])
-    p result
-    assert(true, 'Commit not successful')
+    assert(ProjectUtility.new.run(['-s', 'Test proj_util']), 'Commit not successful')
   end
-  #
+
   # def test_commit_build
   #   assert_equal(true, ProjectUtility.new.run(['-bs', 'Test build proj_util']))
   # end
-  #
+
   # def test_commit_install
   #   assert_equal(true, ProjectUtility.new.run(['-is', 'Test build install proj_util']))
   # end
-  #
+
   # def test_commit_push
   #   assert_equal(true, ProjectUtility.new.run(['-ps', 'Test build push proj_util']))
   # end
