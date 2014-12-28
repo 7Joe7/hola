8e56680debe62988eaf545929e6f9b2b7ba09c0a
Joe <josef.erneker@gmail.com>
2014-12-28 11:55:36 +0100

    Test build push proj_util

--- lib/joe_utils/utils/project_utility.rb
+++ lib/joe_utils/utils/project_utility.rb
@@ -1,4 +1,4 @@
-require '/Users/Joe/Documents/Work/joe_utils/lib/joe_utils.rb'
+require 'joe_utils.rb'
 
 class ProjectUtility
 
@@ -66,7 +66,7 @@ Text behind the command will be considered a message for git.'
   def run(input = nil)
     options = process_input(input || ARGV)
     success = true
-    success = rake_test unless options[:skip_tests]
+    # success = rake_test unless options[:skip_tests]
     success &&= commit_git(options[:message])
     success &&= commit_github if options[:github]
     success &&= deploy_heroku if options[:heroku]
