0cfff99eaabb23c413a20e8de8a0ef9357a7606c
Joe <josef.erneker@gmail.com>
2014-12-28 11:52:41 +0100

    Test build push proj_util

--- test/test_proj_util.rb
+++ test/test_proj_util.rb
@@ -4,23 +4,23 @@ require 'joe_utils/utils/project_utility.rb'
 class JoeUtilsTest < Minitest::Test
 
   def test_commit
-    assert_equal(ProjectUtility.new.run(['-s', 'Test proj_util']), true)
+    assert_equal(true, ProjectUtility.new.run(['-s', 'Test proj_util']))
   end
 
   def test_commit_build
-    assert_equal(ProjectUtility.new.run(['-bs', 'Test build proj_util']), true)
+    assert_equal(true, ProjectUtility.new.run(['-bs', 'Test build proj_util']))
   end
 
   def test_commit_install
-    assert_equal(ProjectUtility.new.run(['-is', 'Test build install proj_util']), true)
+    assert_equal(true, ProjectUtility.new.run(['-is', 'Test build install proj_util']))
   end
 
   def test_commit_push
-    assert_equal(ProjectUtility.new.run(['-ps', 'Test build push proj_util']), true)
+    assert_equal(true, ProjectUtility.new.run(['-ps', 'Test build push proj_util']))
   end
 
   def test_gem_util
-    assert_equal(ProjectUtility.new.run(['ips', 'Test build install push proj_util']), true)
+    assert_equal(true, ProjectUtility.new.run(['ips', 'Test build install push proj_util']))
   end
 
   # def test_commit_install_push
