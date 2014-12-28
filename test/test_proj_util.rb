2adaae19f6a3868478d63ede97b1cc94b4eb8b5d
Joe <josef.erneker@gmail.com>
2014-12-28 15:20:50 +0100

    Test proj_util

--- test/test_proj_util.rb
+++ test/test_proj_util.rb
@@ -6,22 +6,22 @@ class JoeUtilsTest < Minitest::Test
   def test_commit
     assert_equal(true, ProjectUtility.new.run(['-s', 'Test proj_util']))
   end
+  #
+  # def test_commit_build
+  #   assert_equal(true, ProjectUtility.new.run(['-bs', 'Test build proj_util']))
+  # end
+  #
+  # def test_commit_install
+  #   assert_equal(true, ProjectUtility.new.run(['-is', 'Test build install proj_util']))
+  # end
+  #
+  # def test_commit_push
+  #   assert_equal(true, ProjectUtility.new.run(['-ps', 'Test build push proj_util']))
+  # end
 
-  def test_commit_build
-    assert_equal(true, ProjectUtility.new.run(['-bs', 'Test build proj_util']))
-  end
-
-  def test_commit_install
-    assert_equal(true, ProjectUtility.new.run(['-is', 'Test build install proj_util']))
-  end
-
-  def test_commit_push
-    assert_equal(true, ProjectUtility.new.run(['-ps', 'Test build push proj_util']))
-  end
-
-  def test_gem_util
-    assert_equal(true, ProjectUtility.new.run(['ips', 'Test build install push proj_util']))
-  end
+  # def test_gem_util
+  #   assert_equal(true, ProjectUtility.new.run(['ips', 'Test build install push proj_util']))
+  # end
 
   # def test_commit_install_push
   #   ProjectUtility.new.run(['-s', 'Test install push proj_util'])
